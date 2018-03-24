7d79c0635a932d758cb5ddbf9ca5913f211d7230
Thyago Schleuss <tschleuss@gmail.com>
2018-03-24 20:12:13 -0300

    chore: VsCode preferences
    - Creating some custom preferences to hide some files

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express 3' });
-});
+    res.send({ express: 'Hello From Express 3' })
+})
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}`))
+
+const shutDown = () => {
+    console.log('Received kill signal, shutting down gracefully')
+    server.close(() => {
+        process.exit(0)
+    })
+}
+
+process.on('SIGTERM', shutDown)
+process.on('SIGINT', shutDown)
